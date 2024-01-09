1c3465e834ecc96a3df99c692df148da3a98a132
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-01-10 02:26:13 +0700

    update env

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,13 +1,9 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 const devEnv = {
 	backend_url: 'http://localhost:8080/api/v1',
 };
 
 const prodEnv: typeof devEnv = {
-	backend_url: 'http://localhost:8080/api/v1',
+	backend_url: 'https://thedatingcoffee.onrender.com/api/v1',
 };
 
-export const env = process.env.NODE_ENV === 'production' ? prodEnv : devEnv;
+export const env = process.env.NODE_ENV === 'development' ? devEnv : prodEnv;

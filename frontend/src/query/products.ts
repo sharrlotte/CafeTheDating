4e24e5e1f5f0cacf5a7db4da56f2ded35d47af0d
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-01-12 15:34:56 +0700

    cleanup

--- frontend/src/query/products.ts
+++ frontend/src/query/products.ts
@@ -1,12 +1,6 @@
-import api from "../api/api";
-import Product, { ProductSort, ProductType } from "../type/Product";
+import api from '../api/api';
+import Product, { ProductSort, ProductType } from '../type/Product';
 
-const getProducts = async (
-  type: ProductType,
-  sort?: ProductSort
-): Promise<Product[]> => {
-  return api
-    .get("/products", { params: { type, sort } })
-    .then((result) => result.data);
-};
-export { getProducts };
+export async function getProducts(type: ProductType, sort?: ProductSort): Promise<Product[]> {
+	return await api.get('/products', { params: { type, sort } }).then((result) => result.data);
+}

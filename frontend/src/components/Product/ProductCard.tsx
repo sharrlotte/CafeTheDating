aae3b50952b77bc6e58a7dc942d61dd2db2745eb
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-01-14 01:17:16 +0700

    fix banner text

--- frontend/src/components/Product/ProductCard.tsx
+++ frontend/src/components/Product/ProductCard.tsx
@@ -10,12 +10,12 @@ export default function ProductCard({ product }: ProductCardProps) {
 	return (
 		<div className='relative'>
 			<img
-				className='w-[400px]'
+				className='w-[400px] object-cover overflow-hidden'
 				onClick={() => addToCart(product)}
 				src={product.image}
 				alt={product.name}
 			></img>
-			<span className='right-5 top-2 absolute text-2xl text-red-500 bg-white p-2 rounded-lg drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] '>{product.tags.map((tag) => translate(tag))}</span>
+			<span className='right-5 top-2 absolute text-2xl text-white bg-green-400 p-2 rounded-lg drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] '>{product.tags.map((tag) => translate(tag))}</span>
 			<div className='grid absolute left-4 bottom-4'>
 				<span className='text-white text-2xl font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]  flex gap-2	'>
 					<span className={cn('text-white text-2xl font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] 	', { 'line-through ': product.discount > 0 })}>{pricy(product.price)}</span>

c807f7fd224b94595d9cd3a28e673601d4332235
Dat-upoverloading <trandinhdat003@gmail.com>
2024-01-15 19:43:45 +0700

    dat commit

--- frontend/src/query/products.ts
+++ frontend/src/query/products.ts
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-import api from "@/api/api";
-import { CreateProductRequest } from "@/components/AddItemsAdmin/AddItemsAdmin";
-import Product, { ProductType, ProductSort } from "@/type/Product";
-
-
-export async function createProduct(request: CreateProductRequest) {
-  return api.post("/products", request);
-}
-=======
 import { CreateProductRequest } from '@/schema/products';
 import api from '../api/api';
 import Product, { ProductSort, ProductType } from '../type/Product';
->>>>>>> 3f8a8ee23fa855accf4d504e71ee2c6be9a80dfe
 export async function getProducts(type: ProductType | undefined, sort?: ProductSort): Promise<Product[]> {
 	return await api.get('/products', { params: { type, sort } }).then((result) => result.data);
 }
